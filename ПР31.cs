72f18f8d233bc65d92dde8ee025bc26c53a818d5
olenipenis <mv8396884@gmail.com>
2025-12-01 15:57:32 +0300

    Add files via upload

---
+++ ПР31.cs
@@ -0,0 +1,17 @@
+namespace MainForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
