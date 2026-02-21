27950da20a09ffab6dd616da8908227d12c4c3a0
Бахарёнок <mv8396884@gmail.com>
2026-02-21 15:10:11 +0300

    Add files via upload

---
+++ LabWork8/LabWork8/LabWork8/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Applicant
+{
+    public string GPA { get; set; }
+    public string SpecifiedGPA { get; set; }
+    public int fullName { get; set; }
+    public double schoolClass { get; set; }
+
+    public Applicant? this[int GPA, int SpecifiedGPA, int schoolClass]
+    {
+        //get
+        //{
+        //    if (GPA > SpecifiedGPA)
+        //        Console.WriteLine("Балл абитуриента выше указанного");
+        //    if (schoolClass > 10)
+        //        Console.WriteLine("Абитуриент закончил одиннадцатый класс");
+        //}
+        //set { }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        //Console.WriteLine($"FullName: {fullName}, class: {schoolClass}, GPA: {GPA}");
+    }
+}
---
+++ LabWork8/LabWork8/ToString.cs
@@ -0,0 +1,11 @@
+public override string ToString()
+	=> $"GPA: {GPA};\n SpecifiedGPA: {SpecifiedGPA};\n fullName: {fullName};\n schoolClass: {schoolClass};";
+
+Console.WriteLine(Applicant.ToString());
+
+public override string ToString()
+{
+	return $"{base.ToString()}";
+}
+
+Console.WriteLine(Applicant.ToString());
